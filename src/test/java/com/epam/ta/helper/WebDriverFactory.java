baeaa75d98f15d85637978afc13962fb084e5bcb
Kornel Filep <kornel.filep@gmail.com>
2023-12-09 10:54:12 +0100

    make execution headless in CI

--- src/test/java/com/epam/ta/helper/WebDriverFactory.java
+++ src/test/java/com/epam/ta/helper/WebDriverFactory.java
@@ -4,10 +4,14 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Objects;
 
 public class WebDriverFactory {
+
+    @Value("${headless:false}")
+    private Boolean headless;
 
     private WebDriver webDriver;
 
@@ -20,7 +24,7 @@ public class WebDriverFactory {
 
     private WebDriver setUpWebDriver() {
         WebDriverManager.chromedriver().setup();
-        return new ChromeDriver(new ChromeOptions().addArguments("--remote-allow-origins=*"));
+        return new ChromeDriver(new ChromeOptions().setHeadless(headless).addArguments("--remote-allow-origins=*"));
     }
 
     public void tearDown() {
